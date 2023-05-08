cc4ad8d10bd3aafab41f7df303bb5c1a4eb7a7ba
Gush <kakirmsplatoon2@gmail.com>
2023-05-08 16:38:23 +0100

    vue installation sucess

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,13 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>How To Install Vue 3 in Laravel 9 with Vite</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+	@vite(['resources/js/app.js','resources/css/app.css'])
+</head>
+<body>
+	<div id="app"></div>
+</body>
+</html>
