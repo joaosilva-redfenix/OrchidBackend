5b357dcbf22ab5f5413e4959c0469f250a0ae759
Gush <kakirmsplatoon2@gmail.com>
2023-05-09 10:33:45 +0100

    tailwindcss setup sucess (chatGPT setup guide) use this commit as a guide to setup tailwindcss

--- resources/js/components/app.vue
+++ resources/js/components/app.vue
@@ -1,3 +1,10 @@
 <template>
-  How To Install Vue 3 in Laravel 9 with Vite - TechvBlogs
+  <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+    <p class="text-3xl text-gray-700 font-bold mb-5">
+      Welcome!
+    </p>
+    <p class="text-gray-500 text-lg">
+      Vue and Tailwind CSS in action
+    </p>
+ </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./resources/**/*.vue",
+    "./resources/**/*.js",
+    "./resources/**/*.jsx",
+    "./resources/**/*.ts",
+    "./resources/**/*.tsx",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
